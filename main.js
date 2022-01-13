5c7974cbd607b1e03b502ed6560b96ff712ac18a
TeeRexNg <lnguyen1410@gmail.com>
2022-01-13 17:31:47 -0500

    add next, prev button for portfolio popup

--- main.js
+++ main.js
@@ -20,6 +20,10 @@
     })
 })();
 
+function bodyScrollingToggle(){
+    document.body.classList.toggle("hidden-scrolling");
+}
+
 /*--------------------------- Portfolio filter and popup ----------------------*/
 (() => {
     const filterContainer = document.querySelector(".portfolio-filter"),
@@ -61,7 +65,99 @@
         //    get the portfolioItem index
         itemIndex = Array.from(portfolioItem.parentElement.children).indexOf(portfolioItem);
         screenshots = portfolioItems[itemIndex].querySelector(".portfolio-item-img img").getAttribute("data-screenshots");
-        console.log(screenshots);
+        //     convert screenshots into array
+        screenshots = screenshots.split(",");
+        if(screenshots.length === 1){
+            prevBtn.style.display="none";
+            nextBtn.style.display="none";
+        }else{
+            prevBtn.style.display="block";
+            nextBtn.style.display="block";
+        }
+        slideIndex = 0;
+        popupToggle();
+        popupSlideshow();
+        popupDetails();
        }
     })
+
+    closeBtn.addEventListener("click", () => {
+        popupToggle();
+        if(projectDetailsContainer.classList.contains("active")){
+            popupDetailsToggle();
+        }
+    })
+
+    function popupToggle(){
+        popup.classList.toggle("open");
+        bodyScrollingToggle();
+    }
+
+    function popupSlideshow(){
+        const imgSrc = screenshots[slideIndex];
+        const popupImg = popup.querySelector(".pp-img");
+        /*activate loader until the popupImg loaded */
+        popup.querySelector(".pp-loader").classList.add("active");
+        popupImg.src = imgSrc;
+        popupImg.onload = () => {
+            // deactivate loader after the popupImg loaded
+            popup.querySelector(".pp-loader").classList.remove("active");
+        }
+        popup.querySelector(".pp-counter").innerHTML = (slideIndex+1) + " of " + screenshots.length;
+    }
+
+    // next slide
+    nextBtn.addEventListener("click", () => {
+        if(slideIndex === screenshots.length - 1){
+            slideIndex = 0;
+        }else {
+            slideIndex++;
+        }
+        popupSlideshow();
+    })
+
+    // prev slide
+    prevBtn.addEventListener("click" , () =>{
+        if(slideIndex === 0){
+            slideIndex = screenshots.length -1;
+        }else {
+            slideIndex--;
+        }
+        popupSlideshow();
+    })
+
+
+    function popupDetails(){
+        // get the project details
+        const details = portfolioItems[itemIndex].querySelector(".portfolio-item-details").innerHTML;
+        // set the project details
+        popup.querySelector(".pp-project-details").innerHTML = details;
+        // get the project title
+        const title = portfolioItems[itemIndex].querySelector(".portfolio-item-title").innerHTML;
+        // set the project title
+        popup.querySelector(".pp-title h2").innerHTML = title;
+        // get the project category
+        const category = portfolioItems[itemIndex].getAttribute("data-category");
+        // set the project category
+        popup.querySelector(".pp-project-category").innerHTML = category.split("-").join(" ");
+        
+    }
+
+    projectDetailsBtn.addEventListener("click", () => {
+        popupDetailsToggle();
+    })
+    function popupDetailsToggle() {
+        if(projectDetailsContainer.classList.contains("active")){
+            projectDetailsBtn.querySelector("i").classList.remove("fa-minus");
+            projectDetailsBtn.querySelector("i").classList.add("fa-plus");
+            projectDetailsContainer.classList.remove("active");
+            projectDetailsContainer.style.maxHeight = 0 + "px";
+        } else{
+            projectDetailsBtn.querySelector("i").classList.remove("fa-plus");
+            projectDetailsBtn.querySelector("i").classList.add("fa-minus");
+            projectDetailsContainer.classList.add("active");
+            projectDetailsContainer.style.maxHeight = projectDetailsContainer.scrollHeight + "px";
+            popup.scrollTo(0, projectDetailsContainer.offsetTop);
+        }
+    }
 })();
